f09dcd80270fc3edbaf64c26860a7882582903af
mirekbartunek <mucus.hooide@gmail.com>
2022-12-04 15:11:43 +0100

    ⠀

--- src/kotlin/day1/day1.kt
+++ src/kotlin/day1/day1.kt
@@ -1,2 +1,12 @@
 package kotlin.day1
 
+import java.io.File
+import java.util.Scanner
+
+fun main() {
+    val file = File("src/kotlin/day1/day1.txt")
+    val scn = Scanner(file)
+    while (scn.hasNextLine()) {
+
+    }
+}
